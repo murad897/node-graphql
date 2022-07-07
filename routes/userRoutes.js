9096c3b8d2fab66931ebca57e77ea13b9052996b
user <m.jamburia.websmart@gmail.comgit config --global user.email m.jamburia.websmart@gmail.com>
2022-07-07 18:10:53 +0400

    mutation edit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,6 +13,6 @@ router.get("/allUsers/:id", userController.get_all_users);
 
 router.get("/editUser", userController.edit_user);
 
-router.post("/uploadImage", auth.verifyToken, userController.set_profile_pic);
+router.post("/uploadImage", userController.set_profile_pic);
 
 module.exports = router;
